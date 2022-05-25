81684390f96e955cc8d6e032565249f09c713af4
Patrick Maul <maulpatrick1996@gmail.com>
2022-05-25 15:05:24 +0200

    add: testlevel code

---
+++ docs/MC_PM_Test.js
@@ -0,0 +1,25 @@
+import MapCreator from "../../core/MapCreator";
+import Background from "../../core/Background";
+import Goomba from "../../models/Goomba";
+
+class TestLevel extends Phaser.Scene {
+  GOOMBA = null;
+
+  preload() {
+    this.background = new Background(this); // Load background
+    MapCreator.preload(this, { MAP_KEY: "First_test_level_PM" });
+  }
+  create() {
+    this.background.create(this);
+    MapCreator.loadLevel(this);
+    this.GOOMBA = new Goomba(this, { SPAWN_X: 350 }); // Create goomba
+    MapCreator.addPhysics(this);
+    MapCreator.createCamera(this);
+  }
+
+  update() {
+    this.GOOMBA.cursorsHandler(this); // Goomba coursor handler
+    MapCreator.update(this, "Preload");
+  }
+}
+export default TestLevel;
