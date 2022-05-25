5133432349451f2cf4951402e018e0fa2bba5ee4
Patrick Maul <maulpatrick1996@gmail.com>
2022-05-25 20:13:57 +0200

    add: test.js (TestLevel) & config.js

---
+++ src/config.example.js
@@ -0,0 +1,6 @@
+// rename to config.js for locale usage
+
+export default {
+  // directly load test level after preload
+  loadTestLevel: true,
+};
---
+++ src/scenes/levels/test.example.js
@@ -0,0 +1,28 @@
+import Goomba from "../../models/Goomba";
+// TODO delete file after project finished:
+// /scenes/levels/test.js
+// config.js                    loadTestLevel
+// /scenes/Preload.js           loadTestLevel
+// /assets/test-tile.png
+// index.js                     TestLevel
+class TestLevel extends Phaser.Scene {
+  preload() {
+    this.load.image("test-tile", "./src/assets/test-tile.png");
+  }
+  create() {
+    this.platforms = this.physics.add.staticGroup();
+    this.GOOMBA = new Goomba(this, { SPAWN_X: 100, SPAWN_Y: this.cameras.main.height / 2 });
+
+    for (let i = 0; i < 10; i++) {
+      this.platforms.create(100 * i, this.cameras.main.height - 64, "test-tile");
+    }
+
+    this.physics.add.collider(this.player, this.platforms);
+  }
+
+  update() {
+    this.GOOMBA.cursorsHandler(this);
+  }
+}
+
+export default TestLevel;
