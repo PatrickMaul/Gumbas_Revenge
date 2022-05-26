0b5957d8118ba3e9ddb746c9dd0185057a6fc2b4
Patrick Maul <maulpatrick1996@gmail.com>
2022-05-26 14:23:42 +0200

    add texts

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -18,6 +18,28 @@ class Menu extends Phaser.Scene {
     this.GOOMBA = new Goomba(this); // Create goomba
     MapCreator.addPhysics(this);
     MapCreator.createCamera(this);
+
+    const gameOverText = this.make.text({
+      x: 510,
+      y: 220,
+      text: "Gumbas Revenge",
+      style: {
+        font: "48px monospace",
+        fill: "#ffffff",
+      },
+    });
+    gameOverText.setOrigin(0.5, 0.5);
+
+    const restartText = this.make.text({
+      x: 505,
+      y: 260,
+      text: "Start Game",
+      style: {
+        font: "12px monospace",
+        fill: "#ffffff",
+      },
+    });
+    restartText.setOrigin(0.5, 0.5);
   }
 
   update() {
