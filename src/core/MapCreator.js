cd3dc0c701449864b8b94f7a620dd6754992b0a6
Patrick Maul <maulpatrick1996@gmail.com>
2022-05-26 15:19:51 +0200

    feat: coins generate in MapCreator

--- src/core/MapCreator.js
+++ src/core/MapCreator.js
@@ -1,4 +1,5 @@
 import { spawnEnemies, handlePlatformEnemyCollision } from "./utils";
+import Coin from "../models/Coin";
 
 /**
  * This class is loading evrything we need for our Level
@@ -60,42 +61,33 @@ class MapCreator {
    * @param {Scene} phaserScene The scene object.
    */
   static addPhysics(phaserScene) {
-    spawnEnemies(phaserScene)
+    spawnEnemies(phaserScene);
 
-    for(let i = 0; i < this.MAP_LAYERS.length; i++) {
-      const layerName = this.MAP_LAYERS[i].name
-      const layer = phaserScene[this.MAP_LAYERS[i].name]
+    for (let i = 0; i < this.MAP_LAYERS.length; i++) {
+      const layerName = this.MAP_LAYERS[i].name;
+      const layer = phaserScene[this.MAP_LAYERS[i].name];
 
-      if(layerName.includes('LM_')) {
+      if (layerName.includes("LM_")) {
         layer.setCollisionByProperty({ collides: true });
       }
 
-      if(layerName.includes('C_')) {
+      if (layerName.includes("C_")) {
         if (layerName.includes("Dead")) {
-          phaserScene.physics.add.collider(
-            phaserScene.player,
-            layer,
-            () => (phaserScene.gameOver = true),
-            null,
-            this
-          );
+          phaserScene.physics.add.collider(phaserScene.player, layer, () => (phaserScene.gameOver = true), null, this);
         } else if (layerName.includes("Finish")) {
-          phaserScene.physics.add.collider(
-            phaserScene.player,
-            layer,
-            () => (this.FINISH = true),
-            null,
-            this
-          );
+          phaserScene.physics.add.collider(phaserScene.player, layer, () => (this.FINISH = true), null, this);
         } else {
           phaserScene.physics.add.collider(phaserScene.player, layer);
         }
       }
 
-      phaserScene.physics.add.collider(layer, phaserScene.enemies, handlePlatformEnemyCollision.bind(phaserScene))
+      phaserScene.physics.add.collider(layer, phaserScene.enemies, handlePlatformEnemyCollision.bind(phaserScene));
     }
-    
+  }
 
+  static loadObjects(phaserScene) {
+    phaserScene.coinManager = new Coin();
+    phaserScene.coinManager = phaserScene.coinManager.create(phaserScene);
   }
 
   /**
@@ -106,7 +98,6 @@ class MapCreator {
     // Camera settings
     phaserScene.cameras.main.startFollow(phaserScene.player, false, 0.1, 0.1, -750, +32);
     phaserScene.cameras.main.zoomTo(2);
-
   }
 
   /**
